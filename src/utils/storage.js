621b5d1e4b52b0348b1071b521c105c06c9d6cd3
gogo <xiaotian.zhang@56qq.com>
2018-02-09 12:17:22 +0800

    feat: 添加storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -2,23 +2,14 @@ import { AsyncStorage } from 'react-native';
 
 // __DEV__ && AsyncStorage.clear();
 
-const get = async (key) => {
-  try {
-    const value = await AsyncStorage.getItem(key);
-    return JSON.parse(value);
-  } catch (error) {
-    console.log(error);
+const get = key => AsyncStorage.getItem(key)
+  .then(value => JSON.parse(value))
+  .catch((e) => {
+    console.log(e);
     return null;
-  }
-};
+  });
 
-const set = async (key, value) => {
-  try {
-    await AsyncStorage.setItem(key, JSON.stringify(value));
-  } catch (error) {
-    console.log(error);
-  }
-};
+const set = (key, value) => AsyncStorage.setItem(key, JSON.stringify(value)).catch(e => console.log(e));
 
 export default {
   get,
