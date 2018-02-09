5d5f7a716c30ac0cffef020da87e3bfb02355bb6
gogo <xiaotian.zhang@56qq.com>
2018-02-09 17:43:18 +0800

    feat: 从缓存中获取书源和章节内容

--- src/models/chapterDetail.js
+++ src/models/chapterDetail.js
@@ -1,12 +1,16 @@
 import services from '../services';
+import storage from '../utils/storage';
 
 const initialState = {
+  storageBook: {}, // 缓存的小说列表数据, { 55eef8b27445ad27755670b9: [{ host: 'book.my716.com', _id: '' }] }
+  storageCapter: {}, // 缓存的小说内容数据, { 55eef8b27445ad27755670b9: { 0: { line: [], content: '', title: '' }, 2: {} } }
   bookSources: [], // 书源列表
   chapters: [], // 章节列表
   chapter: {
     lines: [], // 章节内容每行数组
   },
   url: '', // mp3地址
+  pn: 0, // 页数
   readIndex: -1, // 阅读的行数
   isRead: false, // 是否在阅读
 };
@@ -18,17 +22,40 @@ export default {
 
   subscriptions: {
     setup({ dispatch }) {
+      dispatch({ type: 'getStorageData' });
     }
   },
 
   effects: {
-    // 获取小说列表
-    * getBookSources({ payload: { id } }, { put, call }) {
-      const bookSources = yield call(services.chapterDetail.getBookSources, id);
-      yield put({ type: 'updateState', payload: { bookSources } });
-      yield put({ type: 'getChapters' });
+    // 从缓存中读取小说的数据
+    * getStorageData({ payload }, { put, call }) {
+      const storageBook = yield call(storage.get, 'storageBook');
+      const storageCapter = yield call(storage.get, 'storageCapter');
 
-// console.log('bookSources', bookSources);
+      yield put({
+        type: 'updateState',
+        payload: { storageBook: storageBook || {}, storageCapter: storageCapter || {} },
+      });
+    },
+
+    // 获取小说源列表
+    * getBookSources({ payload: { id } }, { put, call, select }) {
+      const { storageBook } = yield select(({ chapterDetail }) => chapterDetail);
+      let bookSources = [];
+
+      // 首先从缓存中获取数据
+      if (storageBook && storageBook[id]) {
+        bookSources = storageBook[id];
+      // 否则调用接口
+      } else {
+        bookSources = yield call(services.chapterDetail.getBookSources, id);
+        // 存入缓存
+        storageBook[id] = bookSources;
+        yield call(storage.set, 'storageBook', storageBook);
+      }
+
+      yield put({ type: 'updateState', payload: { bookSources, bookId: id } });
+      yield put({ type: 'getChapters' });
     },
 
     // 获取小说章节列表
@@ -36,19 +63,18 @@ export default {
       const { bookSources } = yield select(({ chapterDetail }) => chapterDetail);
       let chapters;
 
+      // 追书神器的书源看不了，所以从1开始取
       for (let i = 1; i < bookSources.length; i += 1) {
-        const results = yield call(services.chapterDetail.getChapters, bookSources[i]._id);
-        console.log('results', results);
-        if (results.chapters && results.chapters.length) {
-          chapters = results.chapters;
-          break;
-        }
+        const bookSource = bookSources[i];
+        const results = yield call(services.chapterDetail.getChapters, bookSource._id);
+        chapters = results.chapters;
+
+        if (chapters && chapters.length) break;
       }
 
       if (!chapters) {
         console.log('没有找到合适的书源！');
       }
-// console.log('chapters', chapters);
 
       yield put({ type: 'updateState', payload: { chapters } });
       yield put({ type: 'getChapter' });
@@ -56,18 +82,30 @@ export default {
 
     // 获取小说章节内容
     * getChapter({ payload }, { put, call, select }) {
-      const { chapters } = yield select(({ chapterDetail }) => chapterDetail);
+      const { chapters, storageCapter, bookId, pn } = yield select(({ chapterDetail }) => chapterDetail);
 
-      const chapter = chapters[0]; // 取第一章
+      const chaptersObj = storageCapter[bookId] || {};
+      let chapter = chapters[pn];
 
-      const { chapter: { body } } = yield call(services.chapterDetail.getChapter, encodeURIComponent(chapter.link));
-      chapter.content = body;
-      chapter.lines = body.split(/\r\n|[\r\n]/);
-// console.log('chapter', chapter);
+      // 首先从缓存中获取数据
+      if (chaptersObj[pn]) {
+        chapter = chaptersObj[pn];
+      // 否则调用接口
+      } else {
+        const { chapter: { body } } = yield call(services.chapterDetail.getChapter, encodeURIComponent(chapter.link));
+        chapter.content = body;
+        chapter.lines = body.split(/\r\n|[\r\n]/);
+
+        // 存入缓存
+        chaptersObj[pn] = chapter;
+        storageCapter[bookId] = chaptersObj;
+        yield call(storage.set, 'storageCapter', storageCapter);
+      }
 
       yield put({ type: 'updateState', payload: { chapter } });
     },
 
+    // 获取语音合成的mp3地址
     * getMp3Url({ payload: { text, index } }, { put, call }) {
       const { url } = yield call(services.chapterDetail.getMp3Url, encodeURIComponent(text));
       yield put({ type: 'updateState', payload: { url, readIndex: index } });
@@ -79,7 +117,9 @@ export default {
       return { ...state, ...payload };
     },
     resetState(state) {
-      return { ...state, ...initialState };
+      // 部分数据不还原
+      const { storageBook, storageCapter, ...others } = initialState;
+      return { ...state, ...others };
     },
   },
 };
